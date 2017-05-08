e1d06458abdeda478791d285fb2a3bb0107c6fd9
dungvt.dev <vutrungdungbk@gmail.com>
2017-05-08 21:56:53 +0700

    fix

--- predictmodule/prediction/series/base.py
+++ predictmodule/prediction/series/base.py
@@ -3,24 +3,25 @@ import pandas as pd
 
 class Series(object):
     name = ''
-    last = None
+    last_secs = None
     data = None
 
-    def get_last_in_second(self):
-        raise NotImplementedError('get_lasst_in_second must be implement')
+    # bien phia tren cua series, dung de query series tiep theo
+    def get_last_max_seconds(self):
+        raise NotImplementedError('get_last_max_seconds must be implement')
 
     def get_pandas_resample_char(self):
         raise NotImplementedError('get_pandas_resample_char must be implement')
 
-    def append(self, points, last):
+    def append(self, points, last_secs):
         if self.data is None:
             self.data = pd.Series(data=points)
-            self.last = last
+            self.last_secs = last_secs
         else:
-            self._append(points, last)
+            self._append(points, last_secs)
 
-    def _append(self, points, last):
-        l = last - self.last
+    def _append(self, points, last_secs):
+        l = last_secs - self.last_secs
         if len(points) > l:
             new = points[len(points) - l:]
         else:
@@ -29,20 +30,24 @@ class Series(object):
             new = bu
         if new:
             self.data = self.data.append(pd.Series(new), ignore_index=True)
-            self.last = last
+            self.last_secs = last_secs
 
     def append_secs(self, secs_tuple):
+        if not secs_tuple:
+            return
         d = pd.DataFrame(secs_tuple)
         d = d.set_index(d[0] * 1000000000)
-        d_secs = d.set_index(d.index.to_datetime())[1]
-        d_min = d_secs.resample(self.get_pandas_resample_char(),
-                                how='mean')
-        self.append(d_min, d_min.index[-1])
+        d_secs = d.set_index(pd.to_datetime(d.index))[1]
+        d_min = d_secs.resample(self.get_pandas_resample_char()).mean()
+        d_min = d_min.interpolate()
+
+        last = d_min.index[-1].value / 1000000000
+        self.append(d_min, last)
 
 
 class SeriesMinute(Series):
-    def get_last_in_second(self):
-        return (last + 1) * 60 - 1
+    def get_last_max_seconds(self):
+        return self.last_secs + 59
 
     def get_pandas_resample_char(self):
         return 'T'

5bab812f5faf6f0a343f23d3d44d798952b8bc78
dungvt.dev <vutrungdungbk@gmail.com>
2017-05-12 11:20:28 +0700

    update ui

---
+++ dashboard/predictionscale/static/predictionscale/js/service_setting.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    var data = $("div#service-data div")
+
+    var default_data = {
+        period: data.data("period"),
+        dataLength: data.data("data-length"),
+        recentPoint: data.data("recent-point"),
+        periodicNumber: data.data("periodic-number"),
+        neuralSize: data.data("neural-size"),
+        predictLength: data.data("predict-length"),
+        updateInPeriod: data.data("update-in-period"),
+    }
+
+    function filldata() {
+        $.each(default_data, function (key, val) {
+            $('form#settingForm input[name="' + key + '"]').val(val);
+        })
+    }
+
+    var btn_refesh = $('form#settingForm button#refresh')
+    btn_refesh.click(function(){
+        filldata();
+    });
+
+    filldata()
+});
